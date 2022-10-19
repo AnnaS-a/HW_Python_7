fb6c81e17e55cbd0cb09ffa4c4f55045c0b9fb53
anna <slukin1@yandex.ru>
2022-10-19 17:07:33 +0700

    input_data

---
+++ input_data.py
@@ -0,0 +1,13 @@
+def surname():
+    return input('Введите фамилию: ')
+     
+def name():
+    return input('Введите имя: ')
+    
+def number_tel():
+    return input('номер +7 номер без пробелов: ')
+    
+def comment():
+    return input('Комментарий: ')
+   
+   
